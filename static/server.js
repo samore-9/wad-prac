8fe6a10688ef5d960a0ccd6f6ea1e47ee150edc7
samore-9 <samore9565@gmail.com>
2026-07-15 22:11:54 +0530

    Correct syntax errors in server.js
    Fix syntax errors in server.js and update login route.

--- static/server.js
+++ static/server.js
@@ -8,7 +8,12 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.send("Its working!");
-       );
+} 
+);
+
+app.post("login/:id", (req, res) => {
+    Steing id = {req.params};
+    );
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
