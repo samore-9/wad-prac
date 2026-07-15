b0a8c5f6f09d2f27ba8a55d992987afa96a803dc
samore-9 <samore9565@gmail.com>
2026-07-15 22:16:31 +0530

    Refactor age field in student model
    Simplified age field definition in student model.

--- CRUD/models/student.js
+++ CRUD/models/student.js
@@ -5,10 +5,7 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  age: {
-    type: Number,
-    required: true
-  },
+  age: Number,
   course: {
     type: String,
     required: true
